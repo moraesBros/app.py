516ac0cd2532912a242606e5b1a2d44c5974d6d6
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-02-02 19:21:02 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,33 +1,29 @@
-from flask import Flask, request, make_response, redirect, abort;
-app = Flask(__name__);
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>';
+     return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name);
-
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent');
-    return '<p>Your browser is {}</p>'.format(user_agent);
-
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    return '<p>Bad request</p>', 400;
-
-@app.route('/objetoresposta')
-def objetoresposta():
-    response = make_response('<h1>This document carries a cookie!</h1>');
-    response.set_cookie('answer', '42');
-    return response
-
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br/');
-
-@app.route('/abortar')
-def abortar():
-    abort(404);
+    return render_template('user.html', name=name)
